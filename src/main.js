8848ccc7cbafe834ed4db0640c6c5d994ad7e153
Lei Jie <839054164@qq.com>
2017-06-03 13:04:11 +0800

    success first

--- src/main.js
+++ src/main.js
@@ -1,34 +1,14 @@
-import _ from 'lodash'
+
 
 const square = (arr) => {
   //console.log(_.range(0, n));
-  var positonArr = [];
-  for(var i = 0;i<arr.length; i++){
-  	for(var j=0; j<arr[i].length; j++){
-  		if(arr[i][j] == 1){
-  			var tempArr = [];
-  			tempArr.push(i);
-  			tempArr.push(j);
-  			positonArr.push(tempArr);
-  		}
-  	}
-  }
-
-  for (var i = 0; i < positonArr.length; i++) {
-  	var x = positonArr[i][0];
-  	var y = positonArr[i][1];
-
-  	vertify(x-1,y-1);
-  	vertify(x-1,y);
-  	vertify(x-1,y+1);
-  	vertify(x,y-1);
-  	vertify(x,y);
-  	vertify(x,y+1);
-  	vertify(x+1,y-1);
-  	vertify(x+1,y);
-  	vertify(x+1,y+1);
-}
-
+var countArr = [];
+for (var i = 0; i < arr.length; i++) {
+	for (var j = 0; j < arr[i].length; j++) {
+		vertify(i,j);
+	};
+};
+console.log(countArr);
 function vertify(x,y){
   	var count = 0;
   	if(arr[x-1]){
@@ -61,22 +41,23 @@ function vertify(x,y){
 	  		count++ ;
 	  	} 		
   	}
-  	// if(!arr[x]){
-  	// 	arr[x] = [];
-  	// }
-  	if(arr[x]){
-	  	if(count < 2){
-	  		arr[x][y] = 0;
-	  	}else if(count > 2){
-	  		arr[x][y] = 1;
-	  	}  	  	
-	 }
-	
   	
+  	if(count > 3 || count < 2){
+  		countArr.push(0);
+  		
+  	}else if(count > 2){
+  		countArr.push(1);
+  		
+  	}else if(count == 2){
+  		countArr.push(arr[x][y]);
+
+  	}  	  	
+	 return countArr;
 
 }
 
-return arr;
+
+return countArr;
 
 };
 
--- test/main.test.js
+++ test/main.test.js
@@ -8,7 +8,7 @@ describe('square', () => {
   it('should return the square of the input number', () => {
     expect(square([[0,0,0],[0,0,0],[0,1,0],[0,1,1]]).toString()).equal('0,0,0,0,0,0,0,1,1,0,1,1');
   });
-  // it('should return the square of the input number', () => {
-  //   expect(square([[1,1,1,1,1],[1,0,0,0,1],[1,0,0,0,1],[1,0,0,0,1],[1,1,1,1,1]]).toString()).equal('0,0,0,0,0,0,0,1,1,0,1,1');
-  // });    
+  it('should return the square of the input number', () => {
+    expect(square([[1,1,1,1,1],[1,0,0,0,1],[1,0,0,0,1],[1,0,0,0,1],[1,1,1,1,1]]).toString()).equal("1,1,1,1,1,1,0,1,0,1,1,1,0,1,1,1,0,1,0,1,1,1,1,1,1");
+  });    
 });
